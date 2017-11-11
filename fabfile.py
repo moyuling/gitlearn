c003df3caa9c40a9b1b49fdd7cc9d231cb252331
Tris Roland <wudiemperor@gmail.com>
2017-11-12 00:25:20 +0800

    Modify fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,2 +1,11 @@
-def hello():
-  print("Hello world")
+from fabric.api import local
+
+def commit():
+  local("git add -p && git commit")
+
+def push():
+  local("git push")
+
+def prepare_deploy():
+  commit()
+  push()
