a1bd78cc706c1f2994c97306bc424a0444b5b1fa
Tris Roland <wudiemperor@gmail.com>
2018-03-17 00:36:09 +0800

    添加 eslint, stylelint 配置

--- a.js
+++ a.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 const isMock = true
 const loveletter = {
 	from: "nick",
 }
-=======
 const dev = 'isLocal';
 const count = 23;
 const name = 'nick';
@@ -14,8 +12,18 @@ const count = 2;
 const name = 'nick';
 
 const hepturn = {
-    name: 'nick',
-    sex: 1
-};
+  name: 'scot'
+  sex: 1,
+}
+function food(
+  bar,
+  baz,
+) {
+  console.log('ok');
+}
+const mi = {
+	name: 'scot',
+	sex: 1,
+}
+
 const cam = x => x * x;
->>>>>>> 65cde6a970ee25242e4218cdac08d2d7dc2b0737
